797ce396a7a0ff016c10ad56d26c95044c490200
nithin <nithinyell@gmail.com>
2017-07-14 13:23:48 -0700

    Initial Commit

---
+++ webServices/services.h
@@ -0,0 +1,13 @@
+//
+//  services.h
+//  webServices
+//
+//  Created by BABA on 7/12/17.
+//  Copyright © 2017 iOSDevs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface services : NSObject
+
+@end
---
+++ webServices/services.m
@@ -0,0 +1,13 @@
+//
+//  services.m
+//  webServices
+//
+//  Created by BABA on 7/12/17.
+//  Copyright © 2017 iOSDevs. All rights reserved.
+//
+
+#import "services.h"
+
+@implementation services
+
+@end
